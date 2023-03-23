9c0399056086e7b0d2345098c0433ccb2f96749c
YeetWritingCode <121581812+YeetWritingCode@users.noreply.github.com>
2023-03-23 23:45:14 +0800

    bug fix

--- GUI_Assignment/web/admin/js/report.js
+++ GUI_Assignment/web/admin/js/report.js
@@ -1,8 +1,8 @@
-function handleMultipleSelect(fromId, toId) {
+function handleMultipleSelect(fromId, toId, input) {
     const selectElement = document.getElementById(fromId);
     const selectedOptions = Array.from(selectElement.selectedOptions);
     const values = selectedOptions.map(option => option.value);
-    const inputElement = document.getElementById(toId);
+    const inputElement = document.getElementById(input);
     inputElement.value = inputElement.value + ',' + values.join(',');
 
     selectedOptions.forEach(option => {
@@ -11,15 +11,15 @@ function handleMultipleSelect(fromId, toId) {
         const newOption = document.createElement('option');
         newOption.value = option.value;
         newOption.text = option.text;
-        const secondSelect = document.getElementById('second-select');
+        const secondSelect = document.getElementById(toId);
         secondSelect.add(newOption);
         option.remove();
     });
 }
 
-function revertMultipleSelect(fromId, toId) {
+function revertMultipleSelect(fromId, toId, input) {
     const selectElement = document.getElementById(fromId);
-    const inputElement = document.getElementById('my-input');
+    const inputElement = document.getElementById(input);
     const secondSelect = document.getElementById(toId);
 
     const selectedOption = Array.from(secondSelect.selectedOptions);
@@ -40,11 +40,11 @@ function revertMultipleSelect(fromId, toId) {
     });
 }
 
-function handleAllSelect(fromId, toId) {
+function handleAllSelect(fromId, toId, input) {
     const selectElement = document.getElementById(fromId);
     const options = Array.from(selectElement.options);
     const values = options.map(option => option.value);
-    const inputElement = document.getElementById('my-input');
+    const inputElement = document.getElementById(input);
     inputElement.value = values.join(',');
 
     options.forEach(option => {
@@ -58,9 +58,9 @@ function handleAllSelect(fromId, toId) {
     });
 }
 
-function revertAllSelect(fromId, toId) {
+function revertAllSelect(fromId, toId, input) {
     const selectElement = document.getElementById(fromId);
-    const inputElement = document.getElementById('my-input');
+    const inputElement = document.getElementById(input);
     const secondSelect = document.getElementById(toId);
 
     const options = Array.from(secondSelect.options);
