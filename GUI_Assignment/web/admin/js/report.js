298b507eca14040408ac2b87d3561d5c95248c25
YeetWritingCode <121581812+YeetWritingCode@users.noreply.github.com>
2023-03-23 23:29:02 +0800

    add feature
    add function to move all option at once

--- GUI_Assignment/web/admin/js/report.js
+++ GUI_Assignment/web/admin/js/report.js
@@ -13,6 +13,7 @@ function handleMultipleSelect() {
         newOption.text = option.text;
         const secondSelect = document.getElementById('second-select');
         secondSelect.add(newOption);
+        option.remove();
     });
 }
 
@@ -35,5 +36,46 @@ function revertMultipleSelect() {
 
         const newValues = currentValues.filter(value => value !== newOption.value);
         inputElement.value = newValues.join(',');
+        option.remove();
+    });
+}
+
+function handleAllSelect() {
+    const selectElement = document.getElementById('my-select');
+    const options = Array.from(selectElement.options);
+    const values = options.map(option => option.value);
+    const inputElement = document.getElementById('my-input');
+    inputElement.value = values.join(',');
+
+    options.forEach(option => {
+        option.style.display = 'none';
+        const newOption = document.createElement('option');
+        newOption.value = option.value;
+        newOption.text = option.text;
+        const secondSelect = document.getElementById('second-select');
+        secondSelect.add(newOption);
+        option.remove();
+    });
+}
+
+function revertAllSelect() {
+    const selectElement = document.getElementById('my-select');
+    const inputElement = document.getElementById('my-input');
+    const secondSelect = document.getElementById('second-select');
+
+    const options = Array.from(secondSelect.options);
+
+    options.forEach(option => {
+        option.style.display = 'none';
+        const newOption = document.createElement('option');
+        newOption.value = option.value;
+        newOption.text = option.text;
+        selectElement.add(newOption);
+
+        const currentValues = inputElement.value.split(',');
+
+        const newValues = currentValues.filter(value => value !== newOption.value);
+        inputElement.value = newValues.join(',');
+        option.remove();
     });
 }
