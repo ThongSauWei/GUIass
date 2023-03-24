307cf0b8b6090654c6953563ac805ec3e0c9cfd5
YeetWritingCode <121581812+YeetWritingCode@users.noreply.github.com>
2023-03-24 18:18:33 +0800

    add report engine components

--- GUI_Assignment/src/java/Controller/ReportController.java
+++ GUI_Assignment/src/java/Controller/ReportController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,119 +18,291 @@ import java.util.regex.Pattern;
  * @author Yeet
  */
 public class ReportController {
-    public static void main(String[] args) {
-        List<HashMap<String, Object>> salesReportResult = salesReport();
 
-// Iterate over the result and print it
-for (HashMap<String, Object> row : salesReportResult) {
-    System.out.println("Product ID: " + row.get("PRODUCT_ID"));
+    private final static String[] SalesColumnName = {
+        "p.product_id",
+        "p.product_name",
+        "SUM(ol.orders_quantity) AS total_units_sold",
+        "SUM(ol.orders_subprice) AS total_revenue"
+    };
+
+    private final static Map<String, String> SalesGroupByOption = new HashMap<String, String>() {
+        {
+            put("p.product_id", "p.product_id, p.product_name");
+        }
+    };
+
+    private final static String[] SalesOrderByOption = {
+        "p.product_id",
+        "p.product_name",
+        "total_units_sold",
+        "total_revenue"
+    };
+
+    String query;
+    String columns;
+    String wheres = "";
+    String groupBy;
+    String orderBy;
+
+    public ReportController(int rptType, String column, String dateFrom, String dateTo, String groupby, String orderby, String status, String acs) {
+
+        if (rptType == 1) {
+            this.columns = decodeSalesColumn(column);
+            this.wheres += dateFrom != null ? dateFrom.equals("") == false ? " AND o.orders_date > '" + dateFrom + "'" : "" : "";
+            this.wheres += dateTo != null ? dateTo.equals("") == false ? " AND o.orders_date < '" + dateTo + "'" : "" : "";
+
+            if (acs == null) {
+                acs = "DESC";
+            }
+
+            if (!acs.equals("1")) {
+                acs = "DESC";
+            } else {
+                acs = "";
+            }
+
+            if (status.equals("1")) {
+                this.wheres += "AND p.product_active = '1'";
+            } else if (status.equals("0")) {
+                this.wheres += "AND p.product_active = '0'";
+            }
+
+            if (groupby.length() > 0) {
+                this.groupBy = " GROUP BY " + decodeSalesGroupBy(ReportController.SalesGroupByOption);
+            } else {
+                this.groupBy = " GROUP BY p.product_id ";
+            }
+            if (orderby.length() > 0) {
+                this.orderBy = " ORDER BY " + decodeSalesOrderBy(orderby, acs);
+            } else {
+                this.orderBy = "";
+            }
+
+            this.query = String.format("SELECT %s "
+                    + "FROM "
+                    + " orders o"
+                    + " JOIN orderlist ol ON o.orders_id = ol.orders_id"
+                    + " JOIN product p ON ol.product_id = p.product_id"
+                    + " WHERE 1 = 1 %s %s %s",
+                    this.columns, this.wheres, this.groupBy, this.orderBy);
+        } else if (rptType == 2) {
+        }
+
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public String getGroup() {
+        return this.groupBy;
+    }
+
+    public static String decodeSalesColumn(String column) {
+        return decoder(column, ReportController.SalesColumnName);
+    }
+
+    public static String decodeSalesOrderBy(String groupby, String acs) {
+        String input = decoder(groupby, ReportController.SalesOrderByOption);
+        String prefix = acs;
+
+        StringBuilder output = new StringBuilder();
+        String[] values = input.split(",");
+        for (int i = 0; i < values.length; i++) {
+            output.append(values[i]).append(" ").append(prefix).append(",");
+        }
+
+        if (output.length() > 0) {
+            output.setLength(output.length() - 1);
+        }
+        return output.toString();
+    }
+
+    private static String decoder(String column, Map<String, String> field) {
+        if (column.equals("")) {
+            return column;
+        }
+        String[] columnCode = decodeInput(column);
+        StringBuilder sb = new StringBuilder();
+        for (String code : columnCode) {
+            String option = field.get(code);
+            if (option != null) {
+                sb.append(option.trim()).append(",");
+            }
+        }
+        if (sb.length() > 0) {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        return sb.toString();
+    }
+
+    public static String salesFormatDisplay(String inputStr) {
+    Map<String, String> columnMap = new HashMap<>();
+    columnMap.put("product_id", "Product Id");
+    columnMap.put("product_name", "Product Name");
+    columnMap.put("total_units_sold", "Total Units Sold");
+    columnMap.put("total_revenue", "Total Revenue");
+
+    String[] parts = inputStr.split(",");
+    StringBuilder outputBuilder = new StringBuilder();
+    boolean hasDesc = false; 
+    boolean hasOrderBy = false;
+    for (String part : parts) {
+        part = part.trim(); 
+        if (part.startsWith("GROUP BY ")) {
+            outputBuilder.append("GROUP BY ");
+            part = part.substring(9); 
+        } else if (part.startsWith("ORDER BY ")) {
+            outputBuilder.append("ORDER BY ");
+            part = part.substring(9); 
+            hasOrderBy = true;
+        }
+
+        String[] columnParts = part.split("\\.");
+        String columnName = columnParts[columnParts.length - 1];
+        
+        if (columnName.endsWith(" DESC")) {
+            columnName = columnName.substring(0, columnName.length() - 5);
+            hasDesc = true;
+        }
+        
+        String formattedName = columnMap.getOrDefault(columnName, columnName);
+        columnParts[columnParts.length - 1] = formattedName;
+
+        if (columnParts.length == 2 && columnParts[0].equals("p")) {
+            columnParts[0] = "";
+        }
+
+        outputBuilder.append(String.join(".", columnParts));
+        outputBuilder.append(", ");
+    }
+
+    outputBuilder.setLength(outputBuilder.length() - 2);
+
+    if (!hasDesc && hasOrderBy) {
+        outputBuilder.append(" ASC");
+    }
+
+    return outputBuilder.toString().replace('.', ' ');
 }
+
+
+
+    public static String decodeSalesGroupBy(Map<String, String> map) {
+        String concatenatedKeys = "," + String.join(",", map.keySet());
+        return decoder(concatenatedKeys, map);
+    }
+
+    private static String decoder(String column, String[] field) {
+        if (column.equals("")) {
+            return column;
+        }
+        String[] columnCode = ReportController.decodeInput(column);
+        column = field[Integer.parseInt(columnCode[0]) - 1];
+        for (int i = 1; i < columnCode.length; i++) {
+            column += "," + field[Integer.parseInt(columnCode[i]) - 1];
+        }
+        return column;
+    }
+
+    public static String[] decodeInput(String input) {
+        String[] parts = input.split(","); // split the input string at commas
+        String[] output = Arrays.copyOfRange(parts, 1, parts.length);
+        return output;
     }
 
     public static List<HashMap<String, Object>> reportSelector(String query) {
         return DbSet.customizeSqlSelect(query);
     }
 
-    public class Report {
-
-        String query;
-
-        public Report(String query) {
-            this.query = query;
-        }
-
-        public List<HashMap<String, Object>> salesReport() {
-            query = "SELECT\n"
-                    + "  p.product_id,\n"
-                    + "  p.product_name,\n"
-                    + "  SUM(ol.orders_quantity) AS total_units_sold,\n"
-                    + "  SUM(ol.orders_subprice) AS total_revenue\n"
-                    + "FROM\n"
-                    + "  orders o\n"
-                    + "  JOIN orderlist ol ON o.orders_id = ol.orders_id\n"
-                    + "  JOIN product p ON ol.product_id = p.product_id\n"
-                    + "WHERE\n"
-                    + "  o.orders_date BETWEEN '2022-01-01' AND '2022-12-31'\n"
-                    + "GROUP BY\n"
-                    + "  p.product_id,\n"
-                    + "  p.product_name\n"
-                    + "ORDER BY\n"
-                    + "  total_revenue DESC\n"
-                    + "";
-            return reportSelector(query);
-        }
-
-        public List<String> getColumnNames(String sqlQuery) {
-            List<String> columnNames = new ArrayList<>();
-            Pattern pattern = Pattern.compile("SELECT\\s+(.*?)\\s+FROM", Pattern.DOTALL);
-            Matcher matcher = pattern.matcher(sqlQuery);
-            if (matcher.find()) {
-                String columns = matcher.group(1);
-                columns = columns.replaceAll("\\n\\s+", " "); // replace line breaks and indentation with spaces
-                for (String column : columns.split(",")) {
-                    column = column.trim();
-                    if (column.contains(" AS ")) {
-                        column = column.split(" AS ")[1];
-                    } else {
-                        column = column.replaceAll("^[^.]*\\.", ""); // remove table name from column name
-                    }
-                    column = column.replaceAll("_", " "); // replace underscores with spaces
-                    String[] words = column.split(" ");
-                    StringBuilder formattedColumn = new StringBuilder();
-                    for (String word : words) {
-                        formattedColumn.append(word.substring(0, 1).toUpperCase()).append(word.substring(1).toLowerCase()).append(" ");
-                    }
-                    columnNames.add(formattedColumn.toString().trim());
-                }
-            }
-            return columnNames;
-        }
+    public List<HashMap<String, Object>> salesReport() {
+        return reportSelector(query);
     }
-    public static List<HashMap<String, Object>> salesReport() {
-            String query = "SELECT\n"
-                    + "  p.product_id,\n"
-                    + "  p.product_name,\n"
-                    + "  SUM(ol.orders_quantity) AS total_units_sold,\n"
-                    + "  SUM(ol.orders_subprice) AS total_revenue\n"
-                    + "FROM\n"
-                    + "  orders o\n"
-                    + "  JOIN orderlist ol ON o.orders_id = ol.orders_id\n"
-                    + "  JOIN product p ON ol.product_id = p.product_id\n"
-                    + "WHERE\n"
-                    + "  o.orders_date BETWEEN '2022-01-01' AND '2022-12-31'\n"
-                    + "GROUP BY\n"
-                    + "  p.product_id,\n"
-                    + "  p.product_name\n"
-                    + "ORDER BY\n"
-                    + "  total_revenue DESC\n"
-                    + "";
-            return reportSelector(query);
-        }
-    
-    public static List<String> getColumnNames(String sqlQuery) {
-            List<String> columnNames = new ArrayList<>();
-            Pattern pattern = Pattern.compile("SELECT\\s+(.*?)\\s+FROM", Pattern.DOTALL);
-            Matcher matcher = pattern.matcher(sqlQuery);
-            if (matcher.find()) {
-                String columns = matcher.group(1);
-                columns = columns.replaceAll("\\n\\s+", " "); // replace line breaks and indentation with spaces
-                for (String column : columns.split(",")) {
-                    column = column.trim();
-                    if (column.contains(" AS ")) {
-                        column = column.split(" AS ")[1];
-                    } else {
-                        column = column.replaceAll("^[^.]*\\.", ""); // remove table name from column name
-                    }
-                    column = column.replaceAll("_", " "); // replace underscores with spaces
-                    String[] words = column.split(" ");
-                    StringBuilder formattedColumn = new StringBuilder();
-                    for (String word : words) {
-                        formattedColumn.append(word.substring(0, 1).toUpperCase()).append(word.substring(1).toLowerCase()).append(" ");
-                    }
-                    columnNames.add(formattedColumn.toString().trim());
-                }
+
+    public List<String> getDynamicColumnNames() {
+        List<String> columnNames = new ArrayList<>();
+        Pattern pattern = Pattern.compile("SELECT\\s+(.*?)\\s+FROM", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(query);
+
+        if (matcher.find()) {
+            String column_names = matcher.group(1);
+            column_names = column_names.replaceAll("\\n\\s+", " ");
+            for (String column : column_names.split(",")) {
+
+                columnNames.add(nameToDisplay(column).toString().trim());
             }
-            return columnNames;
         }
 
+        return columnNames;
+    }
+
+    public String getOrder() {
+        return this.orderBy;
+    }
+
+    public static StringBuilder nameToDisplay(String column) {
+        column = column.trim();
+        if (column.contains(" AS ")) {
+            column = column.split(" AS ")[1];
+        } else {
+            column = column.replaceAll("^[^.]*\\.", "");
+        }
+        column = column.replaceAll("_", " ");
+        String[] words = column.split(" ");
+        StringBuilder formattedColumn = new StringBuilder();
+        for (String word : words) {
+            formattedColumn.append(word.substring(0, 1).toUpperCase()).append(word.substring(1).toLowerCase()).append(" ");
+        }
+        return formattedColumn;
+    }
+
+    public static List<String> getSalesColumnNamesOption() {
+        List<String> names = new ArrayList<>();
+        for (String option : ReportController.SalesColumnName) {
+
+            names.add(nameToDisplay(option).toString().trim());
+        }
+
+        return names;
+    }
+
+    public static List<String> getSalesGroupByNamesOption() {
+        List<String> names = new ArrayList<>();
+        for (String option : SalesGroupByOption.keySet()) {
+            names.add(nameToDisplay(option).toString().trim());
+        }
+        return names;
+    }
+
+    public static List<String> getSalesOrderByNamesOption() {
+        List<String> names = new ArrayList<>();
+        for (String option : ReportController.SalesOrderByOption) {
+
+            names.add(nameToDisplay(option).toString().trim());
+        }
+
+        return names;
+    }
+
+    public String getConditions() {
+        return this.wheres;
+    }
+
+    public String[] parseWhereCondition() {
+        String sql = this.wheres;
+        String[] result = new String[2];
+        int start = sql.indexOf("WHERE") + 5;
+        if (start == -1) {
+            return null;
+        }
+        String condition = sql.substring(start).trim();
+        String[] parts = condition.split("=");
+        if (parts.length != 2) {
+            return null;
+        }
+        result[0] = parts[0].trim();
+        result[1] = parts[1].trim();
+        return result;
+    }
 }
