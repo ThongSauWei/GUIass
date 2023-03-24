3722d6f1ccac23549ce7f765d5d432f943dd677e
YeetWritingCode <121581812+YeetWritingCode@users.noreply.github.com>
2023-03-24 18:21:26 +0800

    bug fix and add formvalid

--- GUI_Assignment/web/admin/js/report.js
+++ GUI_Assignment/web/admin/js/report.js
@@ -3,18 +3,24 @@ function handleMultipleSelect(fromId, toId, input) {
     const selectedOptions = Array.from(selectElement.selectedOptions);
     const values = selectedOptions.map(option => option.value);
     const inputElement = document.getElementById(input);
-    inputElement.value = inputElement.value + ',' + values.join(',');
 
-    selectedOptions.forEach(option => {
-        option.selected = false;
-        option.style.display = 'none';
-        const newOption = document.createElement('option');
-        newOption.value = option.value;
-        newOption.text = option.text;
-        const secondSelect = document.getElementById(toId);
-        secondSelect.add(newOption);
-        option.remove();
-    });
+    if (selectedOptions.length > 0) {
+
+        inputElement.value = inputElement.value + ',' + values.join(',');
+        selectedOptions.forEach(option => {
+            option.selected = false;
+            option.style.display = 'none';
+            const newOption = document.createElement('option');
+            newOption.value = option.value;
+            newOption.text = option.text;
+            const secondSelect = document.getElementById(toId);
+            secondSelect.add(newOption);
+            option.remove();
+        });
+    }
+    const box = document.getElementById(input + "_select");
+    box.style.backgroundColor = "";
+    box.style.borderColor = "";
 }
 
 function revertMultipleSelect(fromId, toId, input) {
@@ -38,6 +44,9 @@ function revertMultipleSelect(fromId, toId, input) {
         inputElement.value = newValues.join(',');
         option.remove();
     });
+    const box = document.getElementById(input + "_select");
+    box.style.backgroundColor = "";
+    box.style.borderColor = "";
 }
 
 function handleAllSelect(fromId, toId, input) {
@@ -45,18 +54,27 @@ function handleAllSelect(fromId, toId, input) {
     const options = Array.from(selectElement.options);
     const values = options.map(option => option.value);
     const inputElement = document.getElementById(input);
-    inputElement.value = values.join(',');
 
-    options.forEach(option => {
-        option.style.display = 'none';
-        const newOption = document.createElement('option');
-        newOption.value = option.value;
-        newOption.text = option.text;
-        const secondSelect = document.getElementById(toId);
-        secondSelect.add(newOption);
-        option.remove();
-    });
+    if (selectElement.options.length > 0) {
+        const newValue = ',' + values.join(',');
+        const originalValue = inputElement.value || '';
+        inputElement.value = originalValue + newValue;
+
+        options.forEach(option => {
+            option.style.display = 'none';
+            const newOption = document.createElement('option');
+            newOption.value = option.value;
+            newOption.text = option.text;
+            const secondSelect = document.getElementById(toId);
+            secondSelect.add(newOption);
+            option.remove();
+        });
+    }
+    const box = document.getElementById(input + "_select");
+    box.style.backgroundColor = "";
+    box.style.borderColor = "";
 }
+
 
 function revertAllSelect(fromId, toId, input) {
     const selectElement = document.getElementById(fromId);
@@ -78,4 +96,38 @@ function revertAllSelect(fromId, toId, input) {
         inputElement.value = newValues.join(',');
         option.remove();
     });
+    const box = document.getElementById(input + "_select");
+    box.style.backgroundColor = "";
+    box.style.borderColor = "";
+}
+
+function validateForm() {
+    const name = document.getElementById("my-input-groupby");
+    const phone_num = document.getElementById("my-input");
+
+    var isValid = true;
+
+    if (isInputFieldEmpty(name)) {
+        isValid = false;
+    }
+    if (isInputFieldEmpty(phone_num)) {
+        isValid = false;
+    }
+
+    return isValid;
+}
+
+function isInputFieldEmpty(inputField) {
+    const selectDiv = document.getElementById(inputField.id + "_select");
+
+    if (inputField.value.trim() === "") {
+        const errorMessage = "This field need value.";
+        selectDiv.style.borderColor = "red";
+        selectDiv.style.backgroundColor = "rgb(220,53,69)";
+        return true;
+    } else {
+        selectDiv.style.borderColor = "";
+        selectDiv.style.backgroundColor = "";
+        return false;
+    }
 }
