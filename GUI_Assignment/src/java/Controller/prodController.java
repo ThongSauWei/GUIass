046dbc16cdc9053c113e009923ea48c16f064070
YeetWritingCode <121581812+YeetWritingCode@users.noreply.github.com>
2023-03-28 21:42:42 +0800

    standardize code

--- GUI_Assignment/src/java/Controller/prodController.java
+++ GUI_Assignment/src/java/Controller/prodController.java
@@ -9,8 +9,6 @@ import DataAccess.Mapper.ProductMapper;
 import Model.Product;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -18,59 +16,36 @@ import java.util.logging.Logger;
  */
 public class prodController {
 
-    public static void main(String[] args) {
-        System.out.println(new prodController().getProds("air"));
-    }
-
     public boolean updateProd(String s_id, String name, String desc, double price, char active) throws SQLException {
         int id = Integer.parseInt(s_id);
-        try {
-            return new DBTable().Product.Update(new ProductMapper(), new Product(id, name, desc, price, active));
-        } catch (SQLException ex) {
-            Logger.getLogger(prodController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return false;
+        return new DBTable().Product.Update(new ProductMapper(), new Product(id, name, desc, price, active));
     }
 
-    public boolean addProd(String name, String desc, double price, char active) {
-        try {
-            return new DBTable().Product.Add(new ProductMapper(), new Product(name, desc, price, active));
-        } catch (SQLException ex) {
-            Logger.getLogger(prodController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return false;
+    public boolean addProd(String name, String desc, double price, char active) throws SQLException {
+        return new DBTable().Product.Add(new ProductMapper(), new Product(name, desc, price, active));
     }
 
-    public Product getLatestProd() {
-        try {
-            return new DBTable().Product.getData(new ProductMapper(), new ArrayList<>(), "SELECT * FROM product ORDER BY product_id desc FETCH FIRST 1 ROWS ONLY").get(0);
-        } catch (SQLException ex) {
-            Logger.getLogger(prodController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
+    public Product getLatestProd() throws SQLException {
+        return new DBTable().Product.getData(new ProductMapper(), new ArrayList<>(), "SELECT * FROM product ORDER BY product_id desc FETCH FIRST 1 ROWS ONLY").get(0);
     }
 
-    public ArrayList<Product> getProds(String search) {
+    public ArrayList<Product> getProds(String search) throws SQLException {
         DBTable dbTable = new DBTable();
-        try {
-            if (search == null) {
-                return dbTable.getProduct().getData(new ProductMapper());
-            } else {
-                ArrayList<Product> sfgs = dbTable.getProduct().getData(new ProductMapper());
-                ArrayList<Product> products = new ArrayList<>();
-                for (int i = 0; i < sfgs.size(); i++) {
-                    if (Integer.toString(sfgs.get(i).getProductId()).contains(search) || sfgs.get(i).getProductName().toLowerCase().contains(search.toLowerCase())) {
-                        products.add(sfgs.get(i));
-                    }
+        if (search == null) {
+            return dbTable.getProduct().getData(new ProductMapper());
+        } else {
+            ArrayList<Product> sfgs = dbTable.getProduct().getData(new ProductMapper());
+            ArrayList<Product> products = new ArrayList<>();
+            for (int i = 0; i < sfgs.size(); i++) {
+                if (Integer.toString(sfgs.get(i).getProductId()).contains(search) || sfgs.get(i).getProductName().toLowerCase().contains(search.toLowerCase())) {
+                    products.add(sfgs.get(i));
                 }
-                return products;
             }
-        } catch (SQLException ex) {
-            return null;
+            return products;
         }
     }
 
-    public ArrayList<Product> getProds(String search, int status) {
+    public ArrayList<Product> getProds(String search, int status) throws SQLException {
         ArrayList<Product> products = this.getProds(search);
         if (status == 1) {
             for (int i = 0; i < products.size(); i++) {
@@ -87,22 +62,17 @@ public class prodController {
                 }
             }
         }
-
         return products;
     }
 
-    public Product getProd(String id) {
+    public Product getProd(String id) throws SQLException {
         return getProd(Integer.parseInt(id));
     }
 
-    public Product getProd(int id) {
-        try {
-            ArrayList<Product> products = new DBTable().Product.getData(new ProductMapper(), id);
-            if (products.size() > 0) {
-                return products.get(0);
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(prodController.class.getName()).log(Level.SEVERE, null, ex);
+    public Product getProd(int id) throws SQLException {
+        ArrayList<Product> products = new DBTable().Product.getData(new ProductMapper(), id);
+        if (!products.isEmpty()) {
+            return products.get(0);
         }
         return null;
     }
