0aeff1933a352565955391b798987d68a447ea32
Yeet <121581812+YeetWritingCode@users.noreply.github.com>
2023-04-20 11:02:36 +0800

    remove unused code

--- GUI_Assignment/src/java/Servlet/ProdMaint.java
+++ GUI_Assignment/src/java/Servlet/ProdMaint.java
@@ -10,8 +10,6 @@ import Model.ImageTable;
 import Model.Product;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
