dd883960be64ca624262db31022b7c6e977498ec
crabbbb <87376058+crabbbb@users.noreply.github.com>
2023-05-11 10:38:28 +0800

    problem fixing

--- GUI_Assignment/src/java/Servlet/HomeServlet.java
+++ GUI_Assignment/src/java/Servlet/HomeServlet.java
@@ -125,21 +125,5 @@ public class HomeServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         db = new DBTable();
-        String companyName = "Company Name is Missing";
-        String adminEmail = "Admin Email is Missing";
-        String copyRight = "CopyRights is Missing";
-        ServletContext context = getServletContext();
-        if (context.getInitParameter("CompanyName") != null) {
-            companyName = context.getInitParameter("CompanyName");
-        }
-        if (context.getInitParameter("AdminEmail") != null) {
-            adminEmail = context.getInitParameter("AdminEmail");
-        }
-        if (context.getInitParameter("CopyRight") != null) {
-            copyRight = context.getInitParameter("CopyRight");
-        }
-        context.setAttribute("companyName", companyName);
-        context.setAttribute("adminEmail", adminEmail);
-        context.setAttribute("copyRight", copyRight);
     }
 }
